f5eb51508801eda0e667a6e1b9a07e91b6e29e97
IliyanDraganov <iliyan982@gmail.com>
2021-02-12 19:12:21 +0200

    Implement transposition table

--- src/main/java/DecisionMaker.java
+++ src/main/java/DecisionMaker.java
@@ -6,81 +6,92 @@ import java.util.List;
 
 public class DecisionMaker {
     private Move nextMove = null;
-    private HashMap<Integer, Double> exploredPositions;
+    private HashMap<Integer, Move> exploredPositions;
     public static final int MAX_AI_DEPTH = 8;
-    public static final double[][] KING_POSITIONAL_COEFFICIENTS= {{-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
-            {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
-            {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
-            {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
-            {-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0},
-            {-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0},
-            {2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0},
-            {2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0}};
-    public static final double[][] QUEEN_POSITIONAL_COEFFICIENTS= {{-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0},
-            {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0},
-            {-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0},
-            {-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5},
-            {0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5},
-            {-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0},
-            {-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0},
-            {-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0}};
-    public static final double[][] ROOK_POSITIONAL_COEFFICIENTS= {{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-            {0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5},
-            {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
-            {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
-            {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
-            {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
-            {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
-            {0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0}};
-    public static final double[][] BISHOP_POSITIONAL_COEFFICIENTS= {{-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0},
-            {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0},
-            {-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0},
-            {-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0},
-            {-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0},
-            {-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0},
-            {-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0},
-            {-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0}};
-    public static final double[][] KNIGHT_POSITIONAL_COEFFICIENTS= {{-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0},
-            {-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0},
-            {-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0},
-            {-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0},
-            {-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0},
-            {-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0},
-            {-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0},
-            {-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0}};
-    public static final double[][] PAWN_POSITIONAL_COEFFICIENTS= {{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                                                    {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0},
-                                                                    {1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 2.0, 1.0},
-                                                                    {0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5},
-                                                                    {0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0},
-                                                                    {0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5},
-                                                                    {0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5},
-                                                                    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}};
+    public static final double[][] KING_POSITIONAL_COEFFICIENTS = { { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0 },
+            { -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0 },
+            { 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0 },
+            { 2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0 } };
+    public static final double[][] QUEEN_POSITIONAL_COEFFICIENTS = { { -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0 },
+            { -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0 },
+            { -1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0 },
+            { -0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5 },
+            { 0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5 },
+            { -1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0 },
+            { -1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0 },
+            { -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0 } };
+    public static final double[][] ROOK_POSITIONAL_COEFFICIENTS = { { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
+            { 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0 } };
+    public static final double[][] BISHOP_POSITIONAL_COEFFICIENTS = {
+            { -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0 },
+            { -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0 },
+            { -1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0 },
+            { -1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0 },
+            { -1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0 },
+            { -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0 },
+            { -1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0 },
+            { -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0 } };
+    public static final double[][] KNIGHT_POSITIONAL_COEFFICIENTS = {
+            { -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0 },
+            { -4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0 },
+            { -3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0 },
+            { -3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0 },
+            { -3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0 },
+            { -3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0 },
+            { -4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0 },
+            { -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0 } };
+    public static final double[][] PAWN_POSITIONAL_COEFFICIENTS = { { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
+            { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 },
+            { 1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 2.0, 1.0 },
+            { 0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5 },
+            { 0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0 },
+            { 0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5 },
+            { 0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5 },
+            { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 } };
 
-    public DecisionMaker(){
+    public DecisionMaker() {
         exploredPositions = new HashMap<>();
     }
 
-    private double getWeight(Piece piece){
-        if(piece.getPieceType() == PieceType.KNIGHT || piece.getPieceType() == PieceType.BISHOP) return 30.0;
-        if(piece.getPieceType() == PieceType.ROOK) return 50.0;
-        if(piece.getPieceType() == PieceType.QUEEN) return 90.0;
-        if(piece.getPieceType() == PieceType.KING) return 900.0;
-        else return 10.0;
+    private double getWeight(Piece piece) {
+        if (piece.getPieceType() == PieceType.KNIGHT || piece.getPieceType() == PieceType.BISHOP)
+            return 30.0;
+        if (piece.getPieceType() == PieceType.ROOK)
+            return 50.0;
+        if (piece.getPieceType() == PieceType.QUEEN)
+            return 90.0;
+        if (piece.getPieceType() == PieceType.KING)
+            return 900.0;
+        else
+            return 10.0;
     }
 
-    private int weightComparator(Move move1, Move move2, Board board){
+    private int weightComparator(Move move1, Move move2, Board board) {
         boolean isCaptureMove1 = Helpers.isCapture(move1, board), isCaptureMove2 = Helpers.isCapture(move2, board);
-        if(isCaptureMove1 && isCaptureMove2){
+        if (isCaptureMove1 && isCaptureMove2) {
             double diff1 = getWeight(board.getPiece(move1.getTo())) - getWeight(board.getPiece(move1.getFrom()));
             double diff2 = getWeight(board.getPiece(move2.getTo())) - getWeight(board.getPiece(move2.getFrom()));
-            if(diff1 > diff2) return -1;
-            if(diff1 < diff2) return 1;
+            if (diff1 > diff2)
+                return -1;
+            if (diff1 < diff2)
+                return 1;
             return 0;
         }
-        if(isCaptureMove1) return -1; //!isCaptureMove2 && isCaptureMove1
-        if(isCaptureMove2) return 1; //isCaptureMove2 && !isCaptureMove1
-        return 0; //!isCaptureMove2 && !isCaptureMove1
+        if (isCaptureMove1)
+            return -1; // !isCaptureMove2 && isCaptureMove1
+        if (isCaptureMove2)
+            return 1; // isCaptureMove2 && !isCaptureMove1
+        return 0; // !isCaptureMove2 && !isCaptureMove1
     }
 
     public double eval(Board board, int depth, Side AIColor) {
@@ -93,7 +104,7 @@ public class DecisionMaker {
 
         double sum = 0.0;
         Piece currentPiece = null;
-        for(Square square : Square.values()){
+        for (Square square : Square.values()) {
             currentPiece = board.getPiece(square);
             if (currentPiece != null) {
                 Side side = currentPiece.getPieceSide();
@@ -125,7 +136,6 @@ public class DecisionMaker {
     }
 
     private double maxValue(Board board, int depth, double alpha, double beta, Side AIColor, int currentMaxDepth) {
-        if(exploredPositions.containsKey(board.hashCode())) return exploredPositions.get(board.hashCode());
         if (isTerminal(board, depth, currentMaxDepth)) {
             return eval(board, depth, AIColor);
         }
@@ -136,6 +146,14 @@ public class DecisionMaker {
 
         List<Move> legalMoves = board.legalMoves();
         legalMoves.sort((move1, move2) -> weightComparator(move1, move2, board));
+        if (exploredPositions.containsKey(board.hashCode())) {
+            Move exploredMove = exploredPositions.get(board.hashCode());
+            int idx = legalMoves.indexOf(exploredMove);
+            if (idx != -1) {
+                legalMoves.remove(idx);
+                legalMoves.add(0, exploredMove);
+            }
+        }
 //              old comparator
 //        {
 //            boolean isCaptureMove1 = Helpers.isCapture(move1, board), isCaptureMove2 = Helpers.isCapture(move2, board);
@@ -152,19 +170,41 @@ public class DecisionMaker {
             }
             board.undoMove();
             if (value >= beta) {
-                this.nextMove = nextMove;
-                return value;
+                break;
             }
             if (value > alpha) {
                 alpha = value;
             }
         }
+        exploredPositions.put(board.hashCode(), nextMove);
+
+//        r.bqkbnr
+//        ppp.pppp
+//        ..n.....
+//        ......N.
+//        ....pP..
+//        ........
+//        PPPP..PP
+//        RNBQKB.R
+//        Side: BLACK
+
+//        r.bqkbnr
+//        .ppppppp
+//        ........
+//        p.......
+//        .nP.P...
+//        ........
+//        PP.PBPPP
+//        RNBQK.NR
+//        Side: BLACK
+
+//        -1094105495
+
         this.nextMove = nextMove;
         return value;
     }
 
     private double minValue(Board board, int depth, double alpha, double beta, Side AIColor, int currentMaxDepth) {
-        if(exploredPositions.containsKey(board.hashCode())) return exploredPositions.get(board.hashCode());
         if (isTerminal(board, depth, currentMaxDepth)) {
             return eval(board, depth, AIColor);
         }
@@ -175,6 +215,14 @@ public class DecisionMaker {
 
         List<Move> legalMoves = board.legalMoves();
         legalMoves.sort((move1, move2) -> weightComparator(move1, move2, board));
+        if (exploredPositions.containsKey(board.hashCode())) {
+            Move exploredMove = exploredPositions.get(board.hashCode());
+            int idx = legalMoves.indexOf(exploredMove);
+            if (idx != -1) {
+                legalMoves.remove(idx);
+                legalMoves.add(0, exploredMove);
+            }
+        }
         for (Move legalMove : legalMoves) {
             board.doMove(legalMove);
             currentValue = maxValue(board, depth + 1, alpha, beta, AIColor, currentMaxDepth);
@@ -184,19 +232,19 @@ public class DecisionMaker {
             }
             board.undoMove();
             if (value <= alpha) {
-                this.nextMove = nextMove;
-                return value;
+                break;
             }
             if (value < beta) {
                 beta = value;
             }
         }
+        exploredPositions.put(board.hashCode(), nextMove);
         this.nextMove = nextMove;
         return value;
     }
 
     public boolean minimaxDecision(Board board, Side AIColor) {
-        for(int i = 1; i < MAX_AI_DEPTH; i++){
+        for (int i = 1; i < MAX_AI_DEPTH; i++) {
             maxValue(board, 0, Double.MIN_VALUE, Double.MAX_VALUE, AIColor, i);
         }
         return board.doMove(nextMove);
